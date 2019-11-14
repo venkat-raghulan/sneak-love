387cf1b571c06b1867068629e7b31e3d5bab8fca
Venkat <raghul.v@gmail.com>
2019-11-14 16:30:54 +0100

    slider added for filter

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,1 +1,7 @@
-console.log("hello")
+const priceFilter = document.getElementById("price-filter");
+const sizeFilter = document.getElementById("size-filter");
+
+console.log("hello");
+console.log(priceFilter.value);
+
+priceFilter.oninput(console.log(priceFilter.value));
