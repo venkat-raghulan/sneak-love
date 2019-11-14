4a1a15731ef6c2929484760cc99d5e9f728b7a9b
Venkat <raghul.v@gmail.com>
2019-11-14 14:50:39 +0100

    andrew's code merged

--- routes/auth.js
+++ routes/auth.js
@@ -1,3 +1,38 @@
 const express = require("express");
 const router = new express.Router();
+const bcrypt = require("bcrypt");
+const userSchema = require("../models/User");
+router.post("/signup", (req, res, next) => {
+  const user = req.body;
+  userSchema
+    .findOne({ email: user.email })
+    .then(dbRes => {
+      if (dbRes) {
+        res.redirect("/signin");
+        return;
+      }
+      const salt = bcrypt.genSaltSync(10);
+      const hashed = bcrypt.hashSync(user.password, salt);
+      user.password = hashed;
+      userSchema
+        .create(user)
+        .then(() => res.redirect("/signin"))
+        .catch(err => console.log(err));
+    })
+    .catch(dbErr => next(dbErr));
+});
+router.post("/signin", (req, res, next) => {
+  const user = req.body;
+  userSchema
+    .findOne({ email: user.email })
+    .then(dbRes => {
+      if (!dbRes) return res.redirect("/signup");
+      if (bcrypt.compareSync(user.password, dbRes.password)) {
+        return res.redirect("/");
+      } else {
+        return res.redirect("/signin");
+      }
+    })
+    .catch(err => next(err));
+});
 module.exports = router;
