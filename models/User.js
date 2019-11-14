12b98604244d1188dd9493668e885061e230da6d
Venkat <raghul.v@gmail.com>
2019-11-14 15:19:49 +0100

    model changed

--- models/User.js
+++ models/User.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  name: String,
+  firstname: String,
   lastname: String,
   email: String,
   password: String
