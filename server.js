e139c412ec5c36b2a485b6c840090300c64138cd
Venkat <raghul.v@gmail.com>
2019-11-14 10:10:09 +0100

    incoming conflict

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ require("dotenv").config();
 require("./config/mongodb"); // database initial setup
 require("./utils/helpers-hbs"); // utils for hbs templates
 
-
 // base dependencies
 const express = require("express");
 const hbs = require("hbs");
@@ -14,16 +13,16 @@ const mongoose = require("mongoose");
 const MongoStore = require("connect-mongo")(session);
 const cookieParser = require("cookie-parser");
 
-
 // initial config
 app.set("view engine", "hbs");
 app.set("views", __dirname + "/views");
 app.use(express.static("public"));
 hbs.registerPartials(__dirname + "/views/partials");
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 
+console.log("This is war!!");
 
 // SESSION SETUP
 app.use(
@@ -43,12 +42,12 @@ app.locals.site_url = process.env.SITE_URL;
 // used in front end to perform ajax request (var instead of hardcoded)
 
 // CUSTOM MIDDLEWARE
-// check if user is logged in... 
+// check if user is logged in...
 // usecases : conditional display in hbs templates
 // WARNING: this function must be declared AFTER the session setup
 // WARNING: this function must be declared BEFORE app.use(router(s))
 function checkloginStatus(req, res, next) {
-  res.locals.user = req.session.currentUser ? req.session.currentUser : null; 
+  res.locals.user = req.session.currentUser ? req.session.currentUser : null;
   // access this value @ {{user}} or {{user.prop}} in .hbs
   res.locals.isLoggedIn = Boolean(req.session.currentUser);
   // access this value @ {{isLoggedIn}} in .hbs
@@ -58,8 +57,10 @@ function checkloginStatus(req, res, next) {
 function eraseSessionMessage() {
   var count = 0; // initialize counter in parent scope and use it in inner function
   return function(req, res, next) {
-    if (req.session.msg) { // only increment if session contains msg
-      if (count) { // if count greater than 0
+    if (req.session.msg) {
+      // only increment if session contains msg
+      if (count) {
+        // if count greater than 0
         count = 0; // reset counter
         req.session.msg = null; // reset message
       }
@@ -77,7 +78,5 @@ const basePageRouter = require("./routes/index");
 app.use("/", basePageRouter);
 
 const listener = app.listen(process.env.PORT, () => {
-  console.log(
-    `app started at ${process.env.SITE_URL}:${process.env.PORT}`
-  );
+  console.log(`app started at ${process.env.SITE_URL}:${process.env.PORT}`);
 });
