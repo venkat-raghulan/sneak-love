088124964a2c274a807b4612715fe66092fc82ed
Venkat <raghul.v@gmail.com>
2019-11-15 16:13:42 +0100

    IamDoneWithThis

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,29 +5,25 @@ const sizeFilter = document.getElementById("size-filter");
 const productGrid = document.getElementById("products_grid");
 
 function updateList(sneakers) {
-  let tpl1 = `<div id="products_grid" class="products-grid flexed-item">`;
-  let tpl2 = "";
-  sneakers.forEach(sneaker => {
-    tpl2 += `<a href="/one-product/${sneaker._id}" class="product-item-wrapper">
-      <div class="product-img">
-          <img src="${sneaker.image}" alt="${sneaker.description}">
-      </div>
-      <p class="product-name">${sneaker.name}</p>
-      <p class="product-cat">${sneaker.category}</p>
-      <p class="product-price">${sneaker.price}</p><br>Size`;
-
-    let tpl5 = "";
-    sneaker.sizes.forEach(size => {
-      tpl5 += `<p class="product-price">${size}</p>`;
-    });
-
-    tpl2 += `${tpl5}</a>`;
-  });
-
-  let tpl3 = `</div>`;
-  let tpl4 = tpl1 + tpl2 + tpl3;
   productGrid.innerHTML = "";
-  productGrid.innerHTML += tpl4;
+  sneakers.forEach(sneaker => {
+    let sizes = "";
+    sneaker.sizes.forEach(size => {
+      sizes += `<li>EU ${size}</li>`;
+    });
+    const temp = `
+      <a href="/one-product/${sneaker._id}" class="product-item-wrapper">
+        <div class="product-img">
+            <img src="${sneaker.image}" alt="${sneaker.description}">
+        </div>
+        <p class="product-name">${sneaker.name}</p>
+        <p class="product-cat">${sneaker.category}</p>
+        <p class="product-price">Price £${sneaker.price}</p>
+        <span><strong>Available Sizes</strong></span>
+        <ul>${sizes}</ul>
+    </a>`;
+    productGrid.innerHTML += temp;
+  });
 }
 
 function filterByInputPrice(evt) {
